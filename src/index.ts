80f35233ac9fabaa454eb6de3932aa91ca3cd8aa
Jon Q <hello@jonquach.com>
2018-10-31 12:41:10 -0400

    Export GuideContainer and Grid

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ export {default as BoxInspector} from './BoxInspector'
 export {default as Guide} from './Guide'
 export {default as GuideProvider} from './GuideProvider'
 export {default as GuideContext} from './GuideContext'
+export {default as GuideContainer} from './GuideContainer'
+export {default as Grid} from './Grid'
 export {default as Resizer} from './Resizer'
 
 export default Artboard
