e47e3a0167bb6580c26a74aa311cd15172d60c1a
ItsJonQ <itsjonq@gmail.com>
2018-10-31 08:56:03 -0400

    Adjusted files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+export {default as Artboard} from './Artboard'
 export {default as BoxInspector} from './BoxInspector'
 export {default as Guide} from './Guide'
 export {default as GuideProvider} from './GuideProvider'
 export {default as GuideContext} from './GuideContext'
+export {default as Resizer} from './Resizer'
